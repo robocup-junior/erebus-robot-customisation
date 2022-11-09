9a8d406f86b6d345dae87110f1e9c8a9cb836316
Alfred Roberts <robertsalfie14@gmail.com>
2022-11-09 21:46:16 +0000

    Fix distance sensor not being removed correctly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -63,6 +63,7 @@ export class AppComponent implements AfterViewInit {
   checkboxValues = {}
   checkboxImport = {}
   
+  renderDeviceUpdate: Device;
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) {
   }
@@ -159,16 +160,31 @@ export class AppComponent implements AfterViewInit {
     return this.cost + (value) <= this.budget
   }
 
-  onWheelSliderChange($event) {
-    if (this.withinBudget((parseInt($event.value) * this.wheelCost) - (this.previousWheelNumber * this.wheelCost))){
-      this.previousWheelNumber = this.numberOfWheels;
+  addRenderDevice(device: Device): Promise<unknown> {
+    this.selectedDevices[device.dictName] = device;
+    this.renderDeviceUpdate = device;
+    return new Promise(resolve => { setTimeout(() => resolve('added')), 10000 });
+  }
+
+  destroyRenderDevice(device: Device): Promise<unknown> {
+    device.type = 'sub';
+    this.renderDeviceUpdate = {...device};
+    delete this.selectedDevices[device.dictName];
+    return new Promise(resolve => { setTimeout(() => resolve('destroyed')), 10000 });
+  }
+
+  onWheelSliderChange($event): void {
+    this.previousWheelNumber = this.numberOfWheels;
+    if (this.withinBudget((parseInt($event.value) * this.wheelCost) - (this.previousWheelNumber * this.wheelCost))) {
       this.numberOfWheels = parseInt($event.value);
       this.wheelsIterator = Array(this.numberOfWheels).fill(0);
-      if (this.previousWheelNumber - this.numberOfWheels > 0){
-        //decreased slider
-        //console.log("Wheel " + this.previousWheelNumber)
-        this.selectedDevices["Wheel " + this.previousWheelNumber].type = "sub";
-        this.addSelectedComponent(this.selectedDevices[ "Wheel " + this.previousWheelNumber]);
+      if (this.previousWheelNumber - this.numberOfWheels > 0) {        
+        let numWheelToRmv = this.previousWheelNumber - this.numberOfWheels;
+        for (let i = this.previousWheelNumber; i > this.previousWheelNumber - numWheelToRmv; i --) {
+          //decreased slider
+          this.selectedDevices["Wheel " + i].type = "sub";
+          this.destroyRenderDevice(this.selectedDevices["Wheel " + i]);
+        }
       }
       this.cost += (this.numberOfWheels * this.wheelCost) - (this.previousWheelNumber * this.wheelCost)
     } else {
@@ -183,10 +199,13 @@ export class AppComponent implements AfterViewInit {
       this.previousDistNumber = this.numberOfDists;
       this.numberOfDists = parseInt($event.value);
       this.distsIterator = Array(this.numberOfDists).fill(0);
-      if (this.previousDistNumber - this.numberOfDists > 0){
-        //decreased slider
-        this.selectedDevices["Distance Sensor " + this.previousDistNumber].type = "sub";
-        this.addSelectedComponent(this.selectedDevices["Distance Sensor " + this.previousDistNumber]);
+      if (this.previousDistNumber - this.numberOfDists > 0) {
+        let numDistsToRmv = this.previousDistNumber - this.numberOfDists;
+        for (let i = this.previousDistNumber; i > this.previousDistNumber - numDistsToRmv; i --) {
+          //decreased slider
+          this.selectedDevices["Distance Sensor " + i].type = "sub";
+          this.destroyRenderDevice(this.selectedDevices["Distance Sensor " + i]);
+        }
       }
       this.cost += (this.numberOfDists * this.distCost) - (this.previousDistNumber * this.distCost)
     } else {
