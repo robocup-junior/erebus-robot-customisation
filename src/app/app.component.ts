7d3f3ae1430c4f63848a796d578fe36dae698148
Ryo Unemoto <unemoto@mccc.jp>
2023-01-24 07:38:08 +0000

    Revert "Merge branch 'develop/v21' into 'release/v21'"
    This reverts merge request !11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -63,7 +63,6 @@ export class AppComponent implements AfterViewInit {
   checkboxValues = {}
   checkboxImport = {}
   
-  renderDeviceUpdate: Device;
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) {
   }
@@ -160,18 +159,16 @@ export class AppComponent implements AfterViewInit {
     return this.cost + (value) <= this.budget
   }
 
-  onWheelSliderChange($event): void {
-    this.previousWheelNumber = this.numberOfWheels;
-    if (this.withinBudget((parseInt($event.value) * this.wheelCost) - (this.previousWheelNumber * this.wheelCost))) {
+  onWheelSliderChange($event) {
+    if (this.withinBudget((parseInt($event.value) * this.wheelCost) - (this.previousWheelNumber * this.wheelCost))){
+      this.previousWheelNumber = this.numberOfWheels;
       this.numberOfWheels = parseInt($event.value);
       this.wheelsIterator = Array(this.numberOfWheels).fill(0);
-      if (this.previousWheelNumber - this.numberOfWheels > 0) {        
-        let numWheelToRmv = this.previousWheelNumber - this.numberOfWheels;
-        for (let i = this.previousWheelNumber; i > this.previousWheelNumber - numWheelToRmv; i --) {
-          //decreased slider
-          this.selectedDevices["Wheel " + i].type = "sub";
-          this.destoryDevice(this.selectedDevices["Wheel " + i]);
-        }
+      if (this.previousWheelNumber - this.numberOfWheels > 0){
+        //decreased slider
+        //console.log("Wheel " + this.previousWheelNumber)
+        this.selectedDevices["Wheel " + this.previousWheelNumber].type = "sub";
+        this.addSelectedComponent(this.selectedDevices[ "Wheel " + this.previousWheelNumber]);
       }
       this.cost += (this.numberOfWheels * this.wheelCost) - (this.previousWheelNumber * this.wheelCost)
     } else {
@@ -186,13 +183,10 @@ export class AppComponent implements AfterViewInit {
       this.previousDistNumber = this.numberOfDists;
       this.numberOfDists = parseInt($event.value);
       this.distsIterator = Array(this.numberOfDists).fill(0);
-      if (this.previousDistNumber - this.numberOfDists > 0) {
-        let numDistsToRmv = this.previousDistNumber - this.numberOfDists;
-        for (let i = this.previousDistNumber; i > this.previousDistNumber - numDistsToRmv; i --) {
-          //decreased slider
-          this.selectedDevices["Distance Sensor " + i].type = "sub";
-          this.destoryDevice(this.selectedDevices["Distance Sensor " + i]);
-        }
+      if (this.previousDistNumber - this.numberOfDists > 0){
+        //decreased slider
+        this.selectedDevices["Distance Sensor " + this.previousDistNumber].type = "sub";
+        this.addSelectedComponent(this.selectedDevices["Distance Sensor " + this.previousDistNumber]);
       }
       this.cost += (this.numberOfDists * this.distCost) - (this.previousDistNumber * this.distCost)
     } else {
@@ -1350,10 +1344,6 @@ export class AppComponent implements AfterViewInit {
   }
 
   importFromJson(json){
-    this.cost = 0;
-    for (let component in this.selectedDevices) {
-      this.destoryDevice(this.selectedDevices[component]);
-    }
     
     this.distanceSensorValues = []
     this.wheelSensorValues = []
@@ -1387,7 +1377,6 @@ export class AppComponent implements AfterViewInit {
         }
       }
     }
-
     console.log(json)
     this.selectedDevices = json;
 
